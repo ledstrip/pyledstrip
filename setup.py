15baa077ab976e71638417d61f704aa3089a714c
Michael Cipold <github@cipold.de>
2017-09-19 20:41:38 +0200

    module instead of package, add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='Distutils',
+	version='1.0',
+	description='Python interface for streaming color information to a WS2812 LED strip connected to an ESP8266 running the firmware from https://github.com/cnlohr/esp8266ws2812i2s',
+	author='Michael Cipold',
+	author_email='github@cipold.de',
+	url='https://github.com/cipold/pyledstrip',
+	py_modules=['pyledstrip'],
+)
