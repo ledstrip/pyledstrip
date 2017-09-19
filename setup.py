565fe798b5506370e25530bf7af553e614f1e05e
Michael Cipold <github@cipold.de>
2017-09-19 23:04:15 +0200

    fix module name in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(
-	name='Distutils',
+	name='pyledstrip',
 	version='1.0',
 	description='Python interface for streaming color information to a WS2812 LED strip connected to an ESP8266 running the firmware from https://github.com/cnlohr/esp8266ws2812i2s',
 	author='Michael Cipold',
