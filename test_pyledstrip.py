12b45dc28f0637f8d437c14b7537698056b0cbd5
Michael Cipold <github@cipold.de>
2018-03-04 14:16:03 +0100

    format all files with spaces instead of tabs

--- test_pyledstrip.py
+++ test_pyledstrip.py
@@ -7,55 +7,56 @@ from pyledstrip import LedStrip
 
 
 class TestParameters(unittest.TestCase):
-	def test_single(self):
-		strip = LedStrip(led_count=321, ip='123.123.123.123', port=54321, flip=False)
-		self.assertEqual([321], strip._led_counts)
-		self.assertEqual(['123.123.123.123'], strip._ips)
-		self.assertEqual([54321], strip._ports)
-		self.assertEqual([False], strip._flips)
+    def test_single(self):
+        strip = LedStrip(led_count=321, ip='123.123.123.123', port=54321, flip=False)
+        self.assertEqual([321], strip._led_counts)
+        self.assertEqual(['123.123.123.123'], strip._ips)
+        self.assertEqual([54321], strip._ports)
+        self.assertEqual([False], strip._flips)
 
-	def test_single_lists(self):
-		strip = LedStrip(led_count=[321], ip=['123.123.123.123'], port=[54321], flip=[False])
-		self.assertEqual([321], strip._led_counts)
-		self.assertEqual(['123.123.123.123'], strip._ips)
-		self.assertEqual([54321], strip._ports)
-		self.assertEqual([False], strip._flips)
+    def test_single_lists(self):
+        strip = LedStrip(led_count=[321], ip=['123.123.123.123'], port=[54321], flip=[False])
+        self.assertEqual([321], strip._led_counts)
+        self.assertEqual(['123.123.123.123'], strip._ips)
+        self.assertEqual([54321], strip._ports)
+        self.assertEqual([False], strip._flips)
 
-	def test_double_lists(self):
-		strip = LedStrip(led_count=[321, 1337], ip=['123.123.123.123', '1.1.1.1'], port=[54321, 12345], flip=[True, False])
-		self.assertEqual([321, 1337], strip._led_counts)
-		self.assertEqual(['123.123.123.123', '1.1.1.1'], strip._ips)
-		self.assertEqual([54321, 12345], strip._ports)
-		self.assertEqual([True, False], strip._flips)
+    def test_double_lists(self):
+        strip = LedStrip(led_count=[321, 1337], ip=['123.123.123.123', '1.1.1.1'], port=[54321, 12345],
+                         flip=[True, False])
+        self.assertEqual([321, 1337], strip._led_counts)
+        self.assertEqual(['123.123.123.123', '1.1.1.1'], strip._ips)
+        self.assertEqual([54321, 12345], strip._ports)
+        self.assertEqual([True, False], strip._flips)
 
-	def test_partial_1(self):
-		strip = LedStrip(led_count=[321, 1337], ip='123.123.123.123', port=54321, flip=False)
-		self.assertEqual([321], strip._led_counts)
-		self.assertEqual(['123.123.123.123'], strip._ips)
-		self.assertEqual([54321], strip._ports)
-		self.assertEqual([False], strip._flips)
+    def test_partial_1(self):
+        strip = LedStrip(led_count=[321, 1337], ip='123.123.123.123', port=54321, flip=False)
+        self.assertEqual([321], strip._led_counts)
+        self.assertEqual(['123.123.123.123'], strip._ips)
+        self.assertEqual([54321], strip._ports)
+        self.assertEqual([False], strip._flips)
 
-	def test_partial_2(self):
-		strip = LedStrip(led_count=321, ip=['123.123.123.123', '1.1.1.1'], port=54321, flip=False)
-		self.assertEqual([321, 321], strip._led_counts)
-		self.assertEqual(['123.123.123.123', '1.1.1.1'], strip._ips)
-		self.assertEqual([54321, 54321], strip._ports)
-		self.assertEqual([False, False], strip._flips)
+    def test_partial_2(self):
+        strip = LedStrip(led_count=321, ip=['123.123.123.123', '1.1.1.1'], port=54321, flip=False)
+        self.assertEqual([321, 321], strip._led_counts)
+        self.assertEqual(['123.123.123.123', '1.1.1.1'], strip._ips)
+        self.assertEqual([54321, 54321], strip._ports)
+        self.assertEqual([False, False], strip._flips)
 
-	def test_partial_3(self):
-		strip = LedStrip(led_count=321, ip='123.123.123.123', port=[54321, 12345], flip=False)
-		self.assertEqual([321, 321], strip._led_counts)
-		self.assertEqual(['123.123.123.123', '123.123.123.123'], strip._ips)
-		self.assertEqual([54321, 12345], strip._ports)
-		self.assertEqual([False, False], strip._flips)
+    def test_partial_3(self):
+        strip = LedStrip(led_count=321, ip='123.123.123.123', port=[54321, 12345], flip=False)
+        self.assertEqual([321, 321], strip._led_counts)
+        self.assertEqual(['123.123.123.123', '123.123.123.123'], strip._ips)
+        self.assertEqual([54321, 12345], strip._ports)
+        self.assertEqual([False, False], strip._flips)
 
-	def test_partial_4(self):
-		strip = LedStrip(led_count=321, ip='123.123.123.123', port=54321, flip=[True, False])
-		self.assertEqual([321], strip._led_counts)
-		self.assertEqual(['123.123.123.123'], strip._ips)
-		self.assertEqual([54321,], strip._ports)
-		self.assertEqual([True,], strip._flips)
+    def test_partial_4(self):
+        strip = LedStrip(led_count=321, ip='123.123.123.123', port=54321, flip=[True, False])
+        self.assertEqual([321], strip._led_counts)
+        self.assertEqual(['123.123.123.123'], strip._ips)
+        self.assertEqual([54321, ], strip._ports)
+        self.assertEqual([True, ], strip._flips)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
